cbf10a078f857c325a402fc36ff67b3ae5009446
jexists <jexists486@gmail.com>
2021-02-07 20:43:02 +0900

    auth modal fixing

--- src/components/Group/GroupList.js
+++ src/components/Group/GroupList.js
@@ -53,7 +53,7 @@ const GroupList = (Group) => ({ groups, onAddGroup, onAddPlace }) => {
           });
         },
       })
-      })}
+      }
       <ul class="group-list list-group">
         ${ Card({ title: getCardTitle(), className: 'group-list__card', content: groupElements })}
       </ul>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,32 +1,13 @@
 import Button, { BUTTON_COLOR } from '../Button';
-import Modal from '../Modal';
-import ModalAuth from '../Modal-Auth';
 import { showModal } from '../common';
-
-const SIGNIN_MODAL_ID = 'signin-modal';
-const SIGNUP_MODAL_ID = 'signup-modal';
+import AuthModal from './AuthModal';
 
 const Header = ({ className = '' } = {}) => {
-  const onClickLogin = ({ target }) => {
-    showModal({ target, modalSelector: `#${SIGNIN_MODAL_ID}` });
-  };
-
-  const onClickSignup = ({ target }) => {
-    showModal({ target, modalSelector: `#${SIGNUP_MODAL_ID}` });
-  };
+  let newAccount = null;
 
   return `
     <header class=${className}>
-      ${ModalAuth({
-        id: SIGNIN_MODAL_ID,
-        title: '로그인',
-        newAccount: false
-      })}
-      ${ModalAuth({
-        id: SIGNUP_MODAL_ID,
-        title: '회원가입',
-        newAccount: true
-      })}
+    ${AuthModal(newAccount)}
       <nav class="navbar navbar-light bg-light">
         <div class="container-fluid">
           <a class="navbar-brand" href="/"></a>
@@ -35,14 +16,20 @@ const Header = ({ className = '' } = {}) => {
               className: 'header__signin',
               color: BUTTON_COLOR.YELLOW,
               content: '로그인',
-              onClick: onClickLogin,
+              onClick: ({ target }) => {
+                showModal({ target: target.closest('button'), modalSelector: '.signin-modal' });
+                newAccount = false;
+              },
              })}
              ${Button({
               className: 'header__signup',
               color: BUTTON_COLOR.BLUE,
               content: '회원가입',
               borderType: 'outline',
-              onClick: onClickSignup,
+              onClick: ({ target }) => {
+                showModal({ target: target.closest('button'), modalSelector: '.signup-modal' });
+                newAccount = true;
+              },
              })}
           </div>
         </div>
